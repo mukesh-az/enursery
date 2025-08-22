879b270bd558ac4213d5d2e3694bc511d35245e3
mukesh-az <techmukesh@hotmail.com>
2025-08-22 22:38:08 +0100

    Updated css

--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -20,105 +20,101 @@ const ShoppingCart = () => {
 
 
   return (
-    <>
-      <div class="row mt-3">
-        <div class="col-2"></div>
+    <div class="row mt-3" style={{width: '95%'}}>
+      <div class="col-2"></div>
+      <div class="col-8">
+      <h1>Shopping Cart</h1>
 
-        <div class="col-8">
-
-          {cartItems.length}
-
-          <div class="table-responsive">
-            <table class="table table-striped 
+        <div class="table-responsive">
+          <table class="table table-striped 
                     table-bordered table-hover">
-              <thead>
-                <tr>
-                  <th className='col-1 ' scope="col"></th>
-                  <th scope="col">Product Name</th>
-                  <th className='text-center' scope="col">Price</th>
-                  <th className='col-3 text-center' scope="col">Quantity</th>
-                  <th className='col-2 text-center' scope="col">Total</th>
-                  <th className='col-1 text-center' scope="col"></th>
-                </tr>
-              </thead>
-              <tbody>
+            <thead>
+              <tr>
+                <th className='col-1 ' scope="col"></th>
+                <th scope="col">Product Name</th>
+                <th className='text-center' scope="col">Price</th>
+                <th className='col-3 text-center' scope="col">Quantity</th>
+                <th className='col-2 text-center' scope="col">Total</th>
+                <th className='col-1 text-center' scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
 
-                {cartItems.filter(a => a.itemCount > 0).map((plant, i) => (
-                  <tr key={plant.id}>
-                    <td className='text-center'>
-                      <img width={"50px"} height={"40px"} src={plant.image} alt={`Id: ${plant.id} -- ${plant.title}`} />
-                    </td>
-                    <td>
-                      {plant.title}
-                    </td>
-                    <td className='text-center'>{plant.unitPrice}</td>
-                    <td className='text-center'>
-                      <CartAddRemoveButton product={plant} />
+              {cartItems.filter(a => a.itemCount > 0).map((plant, i) => (
+                <tr key={plant.id}>
+                  <td className='text-center'>
+                    <img width={"50px"} height={"40px"} src={plant.image} alt={`Id: ${plant.id} -- ${plant.title}`} />
+                  </td>
+                  <td>
+                    {plant.title}
+                  </td>
+                  <td className='text-center'>{plant.unitPrice}</td>
+                  <td className='text-center'>
+                    <CartAddRemoveButton product={plant} />
 
-                    </td>
-                    <td className='text-center'>{gbpFormatter.format(plant.unitPrice * plant.itemCount)}</td>
-                    <td className='text-center' scope="col">
-                      <button onClick={() =>
-                        dispatch(removeItem(plant))} className="btn btn-danger" >
-                        <Delete />
-                      </button>
-
-                    </td>
-                  </tr>
-                ))}
-                <tr key="totalCartItems">
-                  <td colSpan={4}></td>
-                  <td className='text-center'> <b>{gbpFormatter.format(totalCartValue)}</b></td>
-                  <td scope="col">
-
-
+                  </td>
+                  <td className='text-center'>{gbpFormatter.format(plant.unitPrice * plant.itemCount)}</td>
+                  <td className='text-center' scope="col">
+                    <button onClick={() =>
+                      dispatch(removeItem(plant))} className="btn btn-danger" >
+                      <Delete />
+                    </button>
 
                   </td>
                 </tr>
-                <tr key="cartItemFooter">
-                  <td className='text-center' colSpan={6}>
-                    <Link to="/products">
-                      <Button className='cartButton' variant="outline-primary" size="xxl">
-                        Continue Shopping
-                      </Button>
-                    </Link>
-                    {totalCartValue > 0 && (<>
-                      <Button className='cartButton' variant='outline-danger' onClick={() => {
-                        dispatch(clearAllCartItems()).then(() => {
-                          navigate("/");
-                        });
-                      }}   >
-                        Clear Cart <Delete />
-                      </Button>
-
-
-                      <Button className='cartButton' variant="outline-success" size="xxl"
-                        onClick={() => setModalShow(true)}                    >
-                        Checkout (Comming soon)
-                      </Button>
-                    </>)}
+              ))}
+              <tr key="totalCartItems">
+                <td colSpan={4}></td>
+                <td className='text-center'> <b>{gbpFormatter.format(totalCartValue)}</b></td>
+                <td scope="col">
 
 
 
-                    <PendingModal
-                      show={modalShow}
-                      onHide={() => setModalShow(false)}
-                    />
+                </td>
+              </tr>
+              <tr key="cartItemFooter">
+                <td className='text-center' colSpan={6}>
+                  <Link to="/products">
+                    <Button className='cartButton' variant="outline-primary" size="xxl">
+                      Continue Shopping
+                    </Button>
+                  </Link>
+                  {totalCartValue > 0 && (<>
+                    <Button className='cartButton' variant='outline-danger' onClick={() => {
+                      dispatch(clearAllCartItems()).then(() => {
+                        navigate("/");
+                      });
+                    }}   >
+                      Clear Cart <Delete />
+                    </Button>
 
-                  </td>
-                </tr>
+
+                    <Button className='cartButton' variant="outline-success" size="xxl"
+                      onClick={() => setModalShow(true)}                    >
+                      Checkout (Comming soon)
+                    </Button>
+                  </>)}
 
 
 
-              </tbody>
-            </table>
-          </div>
+                  <PendingModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                  />
 
+                </td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
         </div>
-        <div class="col-2"></div>
 
       </div>
-    </>
+      <div class="col-2"></div>
+
+    </div>
   )
 }
 
